19292e35b5a04f2a586257c66b815b69d98075b8
Harsh Mukhiya <ha981muk@gmail.com>
2025-03-09 02:59:08 +0000

    flask referrals initial

--- app.py
+++ app.py
@@ -3,5 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def referrals():
+    return render_template("referrals.html", title="Hello")
+
+@app.route("/start")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='::', port=5000)  # Listen on all available interfaces
